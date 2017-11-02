38dd0cd82d57f799eab63d2e240458a4030ba422
hamza12358 <sheikh-hamz@hotmail.com>
2017-11-03 04:08:41 +0500

    deleted

--- ip address/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-$ip_address = $_SERVER['REMOTE_ADDR'];
-$ip_block = array('::1','1000.1000');
--- ip address/index.php
+++
@@ -1,7 +0,0 @@
-<?php
- require 'config.php';
- foreach ($ip_block as $ip) {
-     if ($ip == $ip_address){
-         die('your ip is block');
-     }
- }
