d111944f65e6a5ea5411e2d5fec7cbd3d8deaaa6
hamza12358 <sheikh-hamz@hotmail.com>
2017-11-03 04:23:45 +0500

    deleted

--- modified_time.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$time = time();
-$time_now = date('d m y@ H:i:s', $time);
-$time_modified = date('d m y@ H:i:s', strtotime('+1 week 2hours 30minutes'));
-echo "the now is" . $time_now . '<br>' . 'the modified time is' . $time_modified;
-
-
-?>
